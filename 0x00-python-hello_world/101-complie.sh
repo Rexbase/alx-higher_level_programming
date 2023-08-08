b8a3de8810c934bcb690a1807119e41a8bd91cc6
Rexbase <gdnwstomw@gmail.com>
2023-08-08 00:47:18 -0700

    Updated

--- 0x00-python-hello_world/101-complie.sh
+++ 0x00-python-hello_world/101-complie.sh
@@ -1,24 +1,2 @@
 #!/bin/bash
-
-if [ -z "$PYFILE" ]; then
-  echo "Error: Environment variable \$PYFILE is not set."
-  exit 1
-fi
-
-# Extract the filename without extension
-filename=$(basename -- "$PYFILE")
-filename_noext="${filename%.*}"
-
-output_filename="$filename_noext.pyc"
-
-# Compile the Python script
-python -m py_compile "$PYFILE"
-
-# Rename the compiled file
-mv "__pycache__/$filename_noext.cpython-*.pyc" "$output_filename"
-
-# Clean up the __pycache__ directory
-rm -r "__pycache__"
-
-echo "Compilation complete. Compiled file: $output_filename"
-
+python3 -m compileall -b $PYFILE i
