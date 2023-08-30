1ba8104bfb1ce30eb9f6ababe294254d9f148406
Rexbase <gdnwstom@gmail.com>
2023-08-30 13:41:20 +0000

    Updated

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,8 @@
+afe_function(fct, *args):
+    try:
+        r = fct(*args)
+        return r
+    except Exception as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
+        return None
