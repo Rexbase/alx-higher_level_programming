0b68aee1bd0865af71a3163fa5f66d4836648c8e
Rexbase <gdnwstomw@gmail.com>
2023-10-18 06:34:19 -0700

    Updated

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- Create a new database if it dows not yet exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- Crate a new table called 'first_table' if it dows not already exist.
+CREATE TABLE IF NOT EXISTS first_table (
+id InT,
+name VARCHAR(256));
